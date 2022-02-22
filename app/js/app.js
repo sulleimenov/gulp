180072ddf632f230a665dda33fa8b37a43532237
Sulleimenov <sulleimenov@yandex.ru>
2022-02-22 13:22:24 +0600

    Init

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,17 @@
 
 document.addEventListener('DOMContentLoaded', () => {
 
-	// Custom JS
+	/* lazy */
+	const lazyImages = document.querySelectorAll('img[data-src],source[data-srcset]')
+	const loadMapBlock = document.querySelector('.load-map')
+	const windowHeight = document.documentElement.clientHeight
 
+	let lazyImagesPositions = []
+	if (lazyImages > 0) {
+		lazyImages.forEach(img => {
+			if (img.dataset.src || img.dataset.srcset) {
+				lazyImagesPositions.push(img.getBoundingClientRect().top + pageYOffset)
+			}
+		})
+	}
 })
